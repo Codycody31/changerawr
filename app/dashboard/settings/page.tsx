3a31bec421ca06bd77f6b1495e0e4639225b833f
SuperDev <supernova@supers0ft.us>
2025-05-05 19:35:15 -0500

    Hide Security Settings

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -29,7 +29,6 @@ import {
     DialogTitle,
 } from '@/components/ui/dialog';
 import {PasskeysSection} from "@/components/settings/passkeys-section";
-import {SecuritySettings} from "@/components/settings/security-settings";
 
 interface FormState {
     name: string;
