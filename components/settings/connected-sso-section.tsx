4c2ce92f5c0950a02dc7ac4b80e7978c3b482836
SuperDev <supernova@supers0ft.us>
2025-06-28 22:02:18 -0500

    show expiry time as well

--- components/settings/connected-sso-section.tsx
+++ components/settings/connected-sso-section.tsx
@@ -1,3 +1,4 @@
+// components/settings/ConnectedSsoProviders.tsx
 import React from 'react';
 import {Card, CardContent, CardDescription, CardHeader, CardTitle} from '@/components/ui/card';
 import {Badge} from '@/components/ui/badge';
